d5ef875885a1fe2bc833ea580c6d22136c0d95c9
Jasmine Lee <jasmlee@microsoft.com>
2018-07-16 20:17:59 -0700

    Implement DNS client

---
+++ digg.py
@@ -0,0 +1,22 @@
+import sys 
+import socket 
+
+params = sys.argv[1:]
+domain_name = params[0]
+query_type = params[1]
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(("", 8888))
+sock.sendto("hello", ("8.8.8.8", 53))
+sock.recvfrom(100)
+
+"""
+construct a socket
+bind to it so we can receive messages back
+call send
+call receive
+
+open a socket, bind to it, send a request, and see it in wireshark
+"""
+# def digg(site):
+
