a242c9cc65456212f838e69e5fbf54cdc5708452
Jasmine Lee <jasmlee@microsoft.com>
2018-07-24 17:35:27 -0700

    Increase readability and add assertions

--- pcapture.py
+++ pcapture.py
@@ -6,19 +6,30 @@ https://my.bradfieldcs.com/networking/2018-07/overview-exercise/
 """
 
 with open("net.cap", "rb") as f:
+
+    # per-file header according to https://www.tcpdump.org/manpages/pcap-savefile.5.txt
     magic_number_bytes = f.read(4)
     magic_number = binascii.hexlify(magic_number_bytes)
-    print "magic number: {}".format(magic_number)
+    print "Magic number: {}".format(magic_number)
 
     major_version_bytes = f.read(2)
-    major_version = struct.unpack('<H', major_version_bytes)
-    print "major_version: {}".format(major_version)
+    major_version = struct.unpack('<H', major_version_bytes)[0]
+    assert major_version == 2
 
-    minor_version_bytes = f.read(2)
+    minor_version = struct.unpack('<H', f.read(2))[0]
+    assert minor_version == 4
+
     timezone_offset_bytes = f.read(4)
-    timezone_accuracy_bytes = f.read(4)
-    snapshot_length_bytes = f.read(4)
-    byte = f.read(4)  # link-layer
+    assert struct.unpack("<I", timezone_offset_bytes)[0] == 0
+
+    timezone_accuracy = struct.unpack('<I', f.read(4))[0]
+    assert timezone_accuracy == 0
+
+    snapshot_length = struct.unpack('<I', f.read(4))[0]
+    print "Snapshot length: {}".format(snapshot_length)
+
+    byte = struct.unpack('<I', f.read(4))[0]  # link layer header type
+    assert byte == 1  # ethernet type
 
     num_packets = 0
     while byte != "":
@@ -26,17 +37,14 @@ with open("net.cap", "rb") as f:
         byte = f.read(4)  # timestamp
         if byte == "":
             break
-        byte = f.read(4)  # timestamp in ms
-        byte = f.read(4)  # length of captured packet data
-        captured_length_in_bytes = struct.unpack(
-            "<I", byte)  # number of bytes in packet
-        captured_length_in_bytes = captured_length_in_bytes[0]
-        byte = f.read(4)  # Un-truncated length of the packet data
-        untruncated_length_in_bytes = struct.unpack("<I", byte)
-        untruncated_length_in_bytes = untruncated_length_in_bytes[0]
+        # timestamp in ms
+        timestamp_ms = f.read(4)
+        # number of bytes in packet
+        captured_length_in_bytes = struct.unpack("<I", f.read(4))[0]
+        # Un-truncated length of the packet data
+        untruncated_length_in_bytes = struct.unpack("<I", f.read(4))[0]
 
-        if captured_length_in_bytes != untruncated_length_in_bytes:
-            print "Packet was truncated"
+        assert captured_length_in_bytes == untruncated_length_in_bytes
 
         # # parsing the layer 2 ethernet frame
 
@@ -67,14 +75,14 @@ with open("net.cap", "rb") as f:
         # print version_field_header_length
         version_field = version_field_header_length[:len(
             version_field_header_length) / 2]
-        print "version field:" + version_field
+        # print "version field:" + version_field
         # assert version_field == "4" or version_field == "6"
 
         IP_header_length = version_field_header_length[
             len(version_field_header_length) / 2:]
         IP_header_length = int(IP_header_length) % 0xff
         IP_header_length *= 4
-        print "IP_header_length: {}".format(IP_header_length)
+        # print "IP_header_length: {}".format(IP_header_length)
 
         differentiated_services = f.read(1)
         total_length_bytes = f.read(2)
