d11d1d403acdfcd883123f409160f38ea9c95449
Jasmine Lee <jasmlee@microsoft.com>
2018-08-14 19:18:33 -0700

    TCP headers need to be read big-endian

--- pcapture.py
+++ pcapture.py
@@ -4,6 +4,7 @@ import struct
 """
 https://my.bradfieldcs.com/networking/2018-07/overview-exercise/
 """
+requesting_ip = 2600214208  # know by earlier inspection bc its destination port is 80
 
 with open("net.cap", "rb") as f:
 
@@ -14,22 +15,22 @@ with open("net.cap", "rb") as f:
     print "Magic number: {}".format(magic_number)
 
     major_version_bytes = f.read(2)
-    major_version = struct.unpack('<H', major_version_bytes)[0]
+    major_version = struct.unpack('H', major_version_bytes)[0]
     assert major_version == 2
 
-    minor_version = struct.unpack('<H', f.read(2))[0]
+    minor_version = struct.unpack('H', f.read(2))[0]
     assert minor_version == 4
 
     timezone_offset_bytes = f.read(4)
-    assert struct.unpack("<I", timezone_offset_bytes)[0] == 0
+    assert struct.unpack("I", timezone_offset_bytes)[0] == 0
 
-    timezone_accuracy = struct.unpack('<I', f.read(4))[0]
+    timezone_accuracy = struct.unpack('I', f.read(4))[0]
     assert timezone_accuracy == 0
 
-    snapshot_length = struct.unpack('<I', f.read(4))[0]
+    snapshot_length = struct.unpack('I', f.read(4))[0]
     print "Snapshot length: {}".format(snapshot_length)
 
-    byte = struct.unpack('<I', f.read(4))[0]  # link layer header type
+    byte = struct.unpack('I', f.read(4))[0]  # link layer header type
     assert byte == 1  # ethernet type
 
     num_packets = 0
@@ -49,7 +50,7 @@ with open("net.cap", "rb") as f:
 
         # # parsing the layer 2 ethernet frame
 
-        dest, source, ether_type = struct.unpack('<6s6sH', f.read(14))
+        dest, source, ether_type = struct.unpack('6s6sH', f.read(14))
         # dest1, dest2, source1, source2, ether_type = struct.unpack(
         # '<LHLHH', f.read(14))
         # print "dest: {}, source: {}".format(dest1 + dest2, source1+source2)
@@ -66,26 +67,29 @@ with open("net.cap", "rb") as f:
         differentiated_services = f.read(1)
         total_length_bytes = f.read(2)
         # length of datagram payload
-        total_length = struct.unpack("<H", total_length_bytes)[
+        total_length = struct.unpack("H", total_length_bytes)[
             0]
         # print "total length: {}".format(total_length)
         id_field = f.read(2)
         offset_field = f.read(2)
         ttl = f.read(1)
         protocol_field_bytes = f.read(1)
-        protocol_field = struct.unpack("<B", protocol_field_bytes)[0]
+        protocol_field = struct.unpack("B", protocol_field_bytes)[0]
         assert protocol_field == 6  # indicates tcp
 
         header_checksum = f.read(2)
-        source_IP = struct.unpack("<I", f.read(4))[0]
-        destination_IP = struct.unpack("<I", f.read(4))[0]
+        source_IP = struct.unpack("I", f.read(4))[0]
+        destination_IP = struct.unpack("I", f.read(4))[0]
         # should be the same two IPS
-        print "Source IP: {}, destination IP: {}".format(source_IP,
-                                                         destination_IP)
+        #  print "Source IP: {}, destination IP: {}".format(source_IP,
+                                                         # destination_IP)
 
         # parsing TCP headers
         source_port, destination_port, seq_number, ack_number, b12, b13, window_size, \
-            checksum, urgent_pointer = struct.unpack('<HHIIBBHHH', f.read(20))
+            checksum, urgent_pointer = struct.unpack('!HHIIBBHHH', f.read(20))
+
+        # print seq_number
+        # print "Source port: {}, destination port: {}".format(source_port, destination_port)
 
         data_offset = (b12 >> 4)  # * 4  # number of bytes
         options = (data_offset * 4) - 20
